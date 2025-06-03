95bbb9eae83ff58205685e93f629e87a2ceedef4
chengyouling <spadgerlin888@163.com>
2025-06-03 20:16:57 +0800

    [#136] add governance loadbalance demo

---
+++ governance/loadbalance/consumer/src/main/java/com/huaweicloud/sample/ConsumerController.java
@@ -0,0 +1,28 @@
+package com.huaweicloud.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.config.DynamicPropertyFactory;
+
+/**
+ * @Author chengyouling
+ * @Date 2025/6/3
+ **/
+@RestController
+public class ConsumerController {
+
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @RequestMapping("/load-balance")
+  public String getOrder(@RequestParam("id") String id) {
+    return restTemplate.getForObject("http://load-balance-provider/load-balance?id=" + id, String.class);
+  }
+}
